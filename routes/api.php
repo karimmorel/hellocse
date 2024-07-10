6c1b445b4d9ff70e96c757cf518b7d821db88553
Karim Morel <karim-morel@outlook.fr>
2024-07-10 10:47:35 +0700

    Update POST method

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,6 @@ Route::get('/user', function (Request $request) {
 Route::controller(ProfileController::class)->group(function () {
     Route::get('/profiles', 'list');
     Route::post('/profile/create', 'create')->middleware(['auth:sanctum', 'can:create']);
-    Route::put('/profile/{profile}', 'update')->middleware(['auth:sanctum', 'can:update']);
+    Route::post('/profile/{profile}', 'update')->middleware(['auth:sanctum', 'can:update']);
     Route::delete('/profile/{profile}', 'delete')->middleware(['auth:sanctum', 'can:delete']);
 });
